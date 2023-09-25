11bce9330b27a6255e55dfab145fc8f35b5d814d
Matheus Moreira <mthsmoreira59@gmail.com>
2023-09-25 17:25:11 -0300

    adiciona elementos DOM do html

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,9 @@ let currentPage = 1;
 const PER_PAGE = 6;
 
 let response;
+const container = document.getElementById("container");
+const searchBox = document.getElementById("searchBox");
+const prevPageButton = document.getElementById("prevPage");
+const nextPageButton = document.getElementById("nextPage");
+const firstPageButton = document.getElementById("firstPage");
+const lastPageButton = document.getElementById("lastPage");
